e0a2249270b6401fadb62d2b6840fdd6035934d0
Woolf14 <gemilangtirto2002@gmail.com>
2023-05-08 15:07:37 +0700

    front

--- src/pages/customer.vue
+++ src/pages/customer.vue
@@ -1,44 +1,11 @@
 <template>
     <div>
-        <h1>Registration</h1>
-        <form @submit.prevent="submit">
-            <label>
-                Name:
-                <input type="text" v-model="name" required>
-            </label>
-            <br>
-            <label>
-                Email:
-                <input type="email" v-model="email" required>
-            </label>
-            <br>
-            <label>
-                Password:
-                <input type="password" v-model="password" required>
-            </label>
-            <br>
-            <button type="submit">Register</button>
-        </form>
-    </div>
+
 </template>
   
 <script>
 export default {
-    data() {
-        return {
-            name: '',
-            email: '',
-            password: ''
-        };
-    },
-    methods: {
-        submit() {
-            // You can handle the form submission here
-            console.log('Name:', this.name);
-            console.log('Email:', this.email);
-            console.log('Password:', this.password);
-        }
-    }
+    
 };
 </script>
   
--- src/pages/setting.vue
+++ src/pages/setting.vue
@@ -1,28 +1,8 @@
 <template>
-    <div>
-        <h1>Products</h1>
-        <ul>
-            <li v-for="product in products" :key="product.id">
-                {{ product.Product }} - {{ product.Price }}
-            </li>
-        </ul>
-    </div>
+
 </template>
   
 <script>
-import axios from 'axios';
 
-export default {
-    data() {
-        return {
-            products: [],
-        };
-    },
-    mounted() {
-        axios.get('http://localhost:3001/api/products').then((response) => {
-            this.products = response.data;
-        });
-    },
-};
 </script>
   
