fd3c6e334fb9fbe9cc6a147a683a5dd91d5dbe6e
Woolf14 <gemilangtirto2002@gmail.com>
2023-05-09 19:55:47 +0700

    update

--- src/pages/customer.vue
+++ src/pages/customer.vue
@@ -1,44 +1,39 @@
 <template>
     <div>
-        <h1>Registration</h1>
-        <form @submit.prevent="submit">
-            <label>
-                Name:
-                <input type="text" v-model="name" required>
-            </label>
-            <br>
-            <label>
-                Email:
-                <input type="email" v-model="email" required>
-            </label>
-            <br>
-            <label>
-                Password:
-                <input type="password" v-model="password" required>
-            </label>
-            <br>
-            <button type="submit">Register</button>
+        <h2>Upload File</h2>
+        <form @submit.prevent="uploadFile">
+            <div>
+                <label for="image">File:</label>
+                <input type="file" id="image" ref="fileInput" />
+            </div>
+            <button type="submit">Upload</button>
         </form>
     </div>
 </template>
   
 <script>
+import axios from 'axios';
+
 export default {
-    data() {
-        return {
-            name: '',
-            email: '',
-            password: ''
-        };
-    },
     methods: {
-        submit() {
-            // You can handle the form submission here
-            console.log('Name:', this.name);
-            console.log('Email:', this.email);
-            console.log('Password:', this.password);
+        uploadFile() {
+            const fileInput = this.$refs.fileInput;
+            const file = fileInput.files[0];
+            const formData = new FormData();
+            formData.append('image', file);
+
+            axios.post('http://localhost:3000/api/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     }
-};
+}
 </script>
-  
